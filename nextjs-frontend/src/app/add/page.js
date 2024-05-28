5eeaee7fb109f7d505f8381cd212f8c7bc7b0da2
Scott Trocchia <scotttrocchia@gmail.com>
2024-05-27 20:51:16 -0400

    finished html/js for menu add form. yet unformatted [with TailwindCSS].

--- nextjs-frontend/src/app/add/page.js
+++ nextjs-frontend/src/app/add/page.js
@@ -1,7 +1,76 @@
 // src/app/add/page.js
 
+"use client";
+
 import React from "react";
 
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+
+async function createMenu(data) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/menu/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) throw new Error("Failed to create menu data");
+
+  return res.json();
+}
+
 export default function Page() {
-  return <div>Page</div>;
+  const [formData, setFormData] = useState({ name: "", price: "" });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const router = useRouter();
+
+  /**
+   *
+   * @param {Event} evt form submission event (evt for short).
+   */
+  const doFinish = (evt) => {
+    evt.preventDefault();
+    setLoading(true);
+  };
+
+  useEffect(() => {
+    return () => setLoading(false);
+  });
+
+  return (
+    <form onSubmit={doFinish}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={(evt) =>
+            setFormData({ ...formData, name: evt.target.value })
+          }
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="price">Price</label>
+        <input
+          type="number"
+          name="price"
+          value={formData.price}
+          onChange={(evt) =>
+            setFormData({ ...formData, price: evt.target.value })
+          }
+          required
+        />
+      </div>
+      {error && <p>{error}</p>}
+      <div>
+        <button type="submit" disabled={loading}>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
 }
